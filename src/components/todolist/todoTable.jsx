1c858cf07746764557f1ef1f7501fe79f42da57f
caguladamaryangel@gmail.com <caguladamaryangel@gmail.com>
2025-06-07 18:21:25 +0800

    Added mobile view for the task list

--- src/components/todolist/todoTable.jsx
+++ src/components/todolist/todoTable.jsx
@@ -4,43 +4,39 @@ import './todo.css';
 function TodoTable(){
   return (
     <>
-      <div className='hidden overflow-auto rounded-lg shadow md:block'>
-        <table className= {`table-auto w-full`}>
+      {/* Laptop view */}
+      <div className='hidden w-full overflow-auto text-xs rounded-lg shadow md:block'>
+        <table className= {`w-full text-left text-gray-500`}>
             <thead className={`bg-gray-50 border-b-2 border-gray-200 `}>
               <tr>
-                <th  className={`p-3 w-2 text-sm font-semibold tracking-wide text-left`} scope="col">#</th>
-                <th  className={`p-3 w-5 text-sm font-semibold tracking-wide text-left`} scope="col">Date Created</th>
-                <th  className={`p-3 w-100 text-sm font-semibold tracking-wide text-left`} scope="col">Task</th>
-                <th  className={`p-3 text-sm font-semibold tracking-wide text-left`} scope="col">Status</th>
-                <th  className={`p-3 text-sm font-semibold tracking-wide text-left`} scope="col">Actions</th>
-                <th  className={`p-3 w-5 text-sm font-semibold tracking-wide text-left`} scope="col">Target Date</th>
-                <th  className={`p-3 w-5 text-sm font-semibold tracking-wide text-left`} scope="col">Completed</th>
+                <th  className={`p-3 font-semibold tracking-wide text-left`} scope="col">#</th>
+                <th  className={`p-3 font-semibold tracking-wide text-left`} scope="col">Task</th>
+                <th  className={`p-3 font-semibold tracking-wide text-left`} scope="col">Status</th>
+                <th  className={`p-3 font-semibold tracking-wide text-left`} scope="col">Actions</th>
+                <th  className={`p-3 font-semibold tracking-wide text-left`} scope="col">Target Date</th>
+                <th  className={`p-3 font-semibold tracking-wide text-left`} scope="col">Completed</th>
               </tr>
             </thead>
             <tbody>
               {/* Example row */}
               <tr>
-                <td className = {`p-2 text-sm text-gray-700`} >1</td>
-                <td className = {`p-1 text-sm text-gray-700`} >
-                  <input type="date" className="w-full p-1 border rounded" disabled />
+                <td className = {`p-2 text-xs text-gray-700`} >1</td>
+                <td className = {`p-2 text-xs text-gray-700`} >see what solutions we have when it comes to making tailwindcss </td>
+                <td className = {`p-2 text-gray-700`} >
+                  <span className={`css-status p-1.5 text-nowrap text-center uppercase tracking-wider text-yellow-800 bg-yellow-200 rounded-lg `}>In-Progress</span>
                 </td>
-                <td className = {`p-2 text-sm text-gray-700`} >see what solutions we have when it comes to making tailwindcss </td>
-                <td className = {`p-2 text-sm text-gray-700`} >
-                  <span className={`p-1.5 text-xs font-medium uppercase tracking-wider text-yellow-800 bg-yellow-200 rounded-lg `}>In Progress</span>
+                <td className = {`p-2 text-xs text-gray-700`} >
+                  <button className="text-blue-400 hover:text-blue-600">Edit</button>
                 </td>
-                <td className = {`p-2 text-sm text-gray-700`} >
-                  <button className="btn btn-primary btn-sm">Edit</button>
-                  <button className="btn btn-danger btn-sm">Delete</button>
-                </td>
-                <td className = {`p-2 text-sm text-gray-700`} >
+                <td className = {`p-2 text-xs text-gray-700`} >
                   <input type="date" className="w-full p-1 border rounded" disabled/>
                 </td>
-                <td className ={`p-4 text-sm text-gray-700 flex justify-center align-center`} >
+                <td className ={`p-4 text-xs text-gray-700 flex justify-center align-center`} >
                   <button>
-                    <img 
-                      className={`w-4 h-4 hover:scale-110 transition-transform duration-300 hover:bg-yellow-200`} 
-                      src='./images/todolist/star.png' alt="complete-task"
-                    />
+                  <img 
+                    className={`w-4 h-4 opacity-30 outline-black hover:opacity-100 hover:scale-110 transition-transform duration-300`} 
+                    src='./images/todolist/star.png' alt="complete-task"
+                  />
                   </button>
                 </td>
               </tr>
@@ -49,27 +45,23 @@ function TodoTable(){
             <tbody>
               {/* Example row */}
               <tr>
-                <td className = {`p-2 text-sm text-gray-700`} >1</td>
-                <td className = {`p-1 text-sm text-gray-700`} >
-                  <input type="date" className="w-full p-1 border rounded" disabled/>
+                <td className = {`p-2 textxs text-gray-700`} >2</td>
+                <td className = {`p-2 textxs text-gray-700`} >Learn React</td>
+                <td className = {`p-2 textxs text-gray-700`} >
+                <span className={`css-status p-1.5 text-center uppercase tracking-wider text-red-800 bg-red-200 rounded-lg `}>Overdue</span>
                 </td>
-                <td className = {`p-2 text-sm text-gray-700`} >Learn React</td>
-                <td className = {`p-2 text-sm text-gray-700`} >
-                <span className={`p-1.5 text-xs font-medium uppercase tracking-wider text-red-800 bg-red-200 rounded-lg `}>Overdue</span>
+                <td className = {`p-2 text-xs text-gray-700`} >
+                  <button className="text-blue-400 hover:text-blue-600">Edit</button>
                 </td>
-                <td className = {`p-2 text-sm text-gray-700`} >
-                  <button className="btn btn-primary btn-sm">Edit</button>
-                  <button className="btn btn-danger btn-sm">Delete</button>
-                </td>
-                <td className = {`p-2 text-sm text-gray-700`} >
+                <td className = {`p-2 text-xs text-gray-700`} >
                   <input type="date" className="w-full p-1 border rounded" disabled />
                 </td>
-                <td className ={`p-4 text-sm text-gray-700 flex justify-center align-center`} >
+                <td className ={`p-4 flex justify-center align-center`} >
                   <button>
-                    <img 
-                      className={`w-4 h-4 hover:scale-110 transition-transform duration-300 hover:bg-yellow-200`} 
-                      src='./images/todolist/star.png' alt="complete-task"
-                    />
+                  <img 
+                    className={`w-4 h-4 opacity-30 outline-black hover:opacity-100 hover:scale-110 transition-transform duration-300`} 
+                    src='./images/todolist/star.png' alt="complete-task"
+                  />
                   </button>
                 </td>
               </tr>
@@ -78,27 +70,61 @@ function TodoTable(){
         </table>
       </div>
 
+      {/* Mobile view */}
       <div className={`w-full grid grid-cols-1 gap-4 md:hidden`}>
         <div className='p-4 bg-white rounded-lg shadow '>
-          <div>
-            <div>1</div>
-            <div>06/07/2025</div>
-            <div>see what solutions we have when it comes to making tailwindcss</div>
-            <div>In Progress</div>
+          <div className='flex items-center justify-between mb-4'>
+            <div className={`css-status-sm  p-1.5 text-center 
+              uppercase tracking-wider text-yellow-800 bg-yellow-200 rounded-lg `}
+              >In Progress            
+            </div>
+            <div>
+              <button className="text-blue-400 uppercase hover:text-blue-600">Edit</button>
+            </div>
           </div>
+              {/* Task Description */}
+          <div className='flex items-center justify-between mb-4'>
+            <div>See what solutions we have when it comes to making tailwindcss</div>            
+          </div>
+             {/* Target Date and Completion */}
+          <div className='flex items-center justify-between'>
+            <div>06/30/2025</div>
+            <div>
+              <button>
+                <img 
+                  className={`w-4 h-4 opacity-30 outline-black hover:opacity-100 hover:scale-110 transition-transform duration-300`} 
+                  src='./images/todolist/star.png' alt="complete-task"
+                />
+              </button>
+            </div>
+          </div>
+        </div>
 
-          <div>
-            <button className="btn btn-primary btn-sm">Edit</button>
-            <button className="btn btn-danger btn-sm">Delete</button>
+        <div className='p-4 bg-white rounded-lg shadow '>
+          <div className='flex items-center justify-between mb-4'>
+            <div className={`css-status-sm p-1.5 text-center uppercase 
+              tracking-wider text-red-800 bg-red-200 rounded-lg`}
+              >Overdue
+            </div>
+            <div>
+              <button className="text-blue-400 uppercase hover:text-blue-600">Edit</button>
+            </div>
           </div>
-          <div>06/30/2025</div>
-          <div>
-            <button>
-              <img 
-                className={`w-4 h-4 hover:scale-110 transition-transform duration-300 hover:bg-yellow-200`} 
-                src='./images/todolist/star.png' alt="complete-task"
-              />
-            </button>
+              {/* Task Description */}
+          <div className='flex items-center justify-between mb-4'>
+            <div>See what solutions we have when it comes to making tailwindcss</div>            
+          </div>
+             {/* Target Date and Completion */}
+          <div className='flex items-center justify-between'>
+            <div>06/30/2025</div>
+            <div>
+              <button>
+                <img 
+                  className={`w-4 h-4 opacity-30 outline-black hover:opacity-100 hover:scale-110 transition-transform duration-300`} 
+                  src='./images/todolist/star.png' alt="complete-task"
+                />
+              </button>
+            </div>
           </div>
         </div>
       </div>
